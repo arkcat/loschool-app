a341188abbdb5fc491816a9792f3acd25930f1d7
Kim Hyun <arkcat@outlook.com>
2023-10-03 18:11:16 +0900

    Set dynamic force

--- app/manage/raid/page.tsx
+++ app/manage/raid/page.tsx
@@ -1,6 +1,8 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 
+export const dynamic = 'force-dynamic'
+
 export default async function Index() {
   const supabase = createServerComponentClient({ cookies })
 
--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -2,6 +2,8 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import SelectMember from '@/components/SelectMember'
 
+export const dynamic = 'force-dynamic'
+
 export default async function Index() {
   const supabase = createServerComponentClient({ cookies })
   const {
@@ -10,7 +12,7 @@ export default async function Index() {
 
   return (
     <div className="animate-in flex flex-col gap-14 opacity-0 max-w-4xl px-3 py-16 lg:py-24 text-foreground">
-            <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent" />
+      <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent" />
       <div className="flex flex-col gap-8 text-foreground">
         <div>
           <h3 className="font-bold mb-2  min-h-[40px] lg:min-h-[60px] text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center my-12">
