bf102fe6a7ed16e9d20734df2a843ea2e333fa1f
Kim Hyun <arkcat@outlook.com>
2023-11-04 02:16:21 +0900

    Add icon for my raid party.

--- app/page.tsx
+++ app/page.tsx
@@ -6,21 +6,23 @@ import { useRecoilState } from 'recoil'
 import { userAtom } from '../recoil/userAtom'
 import LoginForm from '@/components/LoginForm'
 import { Box, Grid, List, ListItem, ListItemText, Typography } from '@mui/material'
-import Image from 'next/image'
-import logo from './res/logo.png'
 import titleImage from './res/title.png'
-
-import { PartyData, RaidData } from '@/lib/database.types'
+import questIcon from './res/Dungeon_Quest_Icon.png'
+import { PartyData, RaidData, CharacterData } from '@/lib/database.types'
 
 export const dynamic = 'force-dynamic'
+
+interface CombinedData {
+  party: PartyData
+  raid: RaidData
+  members: CharacterData[]
+}
 
 export default function Index() {
   const [showLoginForm, setShowLoginForm] = useState(false)
   const [userState, setUserState] = useRecoilState<any | null>(userAtom)
-  const [memberId, setMemberId] = useState<number>(0)
-  const [partyData, setPartyData] = useState<PartyData[]>([])
-  const [raidData, setRaidData] = useState<RaidData[]>([])
   const [mainComment, setMainComment] = useState<string>('')
+  const [combinedData, setCombinedData] = useState<CombinedData[]>([])
 
   useEffect(() => {
     const fetchMainComment = async () => {
@@ -42,58 +44,60 @@ export default function Index() {
       }
     }
 
-    const fetchUser = async () => {
-      try {
-        if (!userState) {
-          const authSession = supabase.auth.getSession()
-          const currentSession = (await authSession).data.session
-          if (currentSession !== null) {
-            handleUpdateMember(currentSession.user.id)
-          } else {
-            setUserState(null)
-          }
-        }
-      } catch (error: any) {
-        console.error('사용자 정보 가져오기 오류:', error.message)
-      }
-    }
-
     const fetchPartyData = async () => {
-      try {
-        const today = getDayOfWeek()
-        const { data, error } = await supabase
-          .from('Party')
-          .select()
-          .eq('day', today)
-          .order('time')
+      // PartyData 가져오기
+      const today = getDayOfWeek()
+      const { data: partyData, error: partyError } = await supabase
+        .from('Party')
+        .select('*')
+        .eq('day', today)
+        .order('time')
 
-        if (error) {
-          console.error('Error fetching party data:', error)
-        } else {
-          setPartyData(data as PartyData[])
-        }
-      } catch (error: any) {
-        console.error('사용자 정보 가져오기 오류:', error.message)
+      if (partyError) {
+        console.error('Error fetching party data:', partyError);
+        return;
       }
-    }
 
-    const fetchRaidData = async () => {
-      const { data, error } = await supabase
+      // RaidData 가져오기
+      const { data: raidData, error: raidError } = await supabase
         .from('Raid')
-        .select()
+        .select('*')
         .order('id')
 
-      if (data) {
-        setRaidData(data)
-      } else {
-        console.error('Error fetching raid data:', error)
+      if (raidError) {
+        console.error('Error fetching raid data:', raidError);
+        return;
       }
+
+      // MemberData 가져오기
+      const characterIds = partyData.flatMap(party => party.member);
+      const { data: characterData, error: characterError } = await supabase
+        .from('Character')
+        .select('*')
+        .in('id', characterIds);
+
+      if (characterError) {
+        console.error('Error fetching character data:', characterError);
+        return;
+      }
+
+      // 조합된 데이터 생성
+      const combinedData = partyData.map(party => {
+        const raid = raidData[party.raid_id];
+        const members = characterData.filter(character => party.member.includes(character.id));
+
+        return {
+          party,
+          raid: raid,
+          members: members,
+        };
+      });
+
+      setCombinedData(combinedData)
     }
 
-    fetchUser()
     fetchMainComment()
     fetchPartyData()
-    fetchRaidData()
   }, [])
 
   function getDayOfWeek(): number {
@@ -125,17 +129,26 @@ export default function Index() {
   }
 
   interface PartyItemProps {
-    data: PartyData
+    data: CombinedData
   }
+
   const PartyItem: React.FC<PartyItemProps> = ({ data }) => {
-    const raidInfo = raidData[data.raid_id]
-    if (raidInfo === undefined) {
+    if (data === undefined) {
       return <Box></Box>
     }
+    console.log(data)
+    const raidInfo = data.raid
+    const isMy = data.members.filter(char => { return char.member_id === userState.id }).length > 0
+    console.log(userState.id, isMy)
     return (
       <ListItem>
         <ListItemText
-          primary={<Box style={{ fontFamily: 'NanumBarunGothic', fontSize: '20px' }}>{`${raidInfo.raid_name} ${data.time} 시`}</Box>}
+          primary={
+            <Box style={{ display: 'flex', alignItems: 'center', fontFamily: 'NanumBarunGothic', fontSize: '20px' }}>
+              <span style={{ lineHeight: '20px' }}>{`${raidInfo.raid_name} ${data.party.time} 시`}</span>
+              {isMy && <img src={questIcon.src} style={{ width: '25px', marginLeft: '10px' }} />}
+            </Box>
+          }
         />
       </ListItem>
     )
@@ -148,8 +161,7 @@ export default function Index() {
           <img src={titleImage.src} className='title-image' alt="Title" />
         </Box>
         <Box>
-          {
-            !userState &&
+          {!userState &&
             (
               <div className='outlined'>
                 {showLoginForm === false ?
@@ -159,8 +171,7 @@ export default function Index() {
                   </div>
                 }
               </div>
-            )
-          }
+            )}
         </Box>
         <Box>
           {userState &&
@@ -173,8 +184,8 @@ export default function Index() {
                     style={{ fontFamily: 'SUIT-Regular', fontSize: '32px', fontWeight: '600' }}>오늘의 공대</Typography>
                   <div className='main-post'>
                     <List dense>
-                      {partyData.map((data) => (
-                        <PartyItem key={data.id} data={data} />
+                      {combinedData.map((data) => (
+                        <PartyItem key={data.party.id} data={data} />
                       ))}
                     </List>
                   </div>
@@ -185,18 +196,15 @@ export default function Index() {
                     sx={{ mt: 2, mb: 2 }}
                     style={{ fontFamily: 'SUIT-Regular', fontSize: '32px', fontWeight: '600' }}>운영진 공지</Typography>
                   <div className='main-post'>
-                    <Typography style={{ fontFamily: 'NanumBarunGothic', fontSize: '20px', textAlign: 'center' }}>
-                      {mainComment.split('\n').map((line, index) => (
-                        <div key={index}>
-                          {line}
-                        </div>
-                      ))}
-                    </Typography>
+                    {mainComment.split('\n').map((line, index) => (
+                      <Typography key={index} style={{ fontFamily: 'NanumBarunGothic', fontSize: '20px', textAlign: 'center' }}>
+                        {line}
+                      </Typography>
+                    ))}
                   </div>
                 </Grid>
               </Grid>
-            </Box>)
-          }
+            </Box>)}
         </Box>
       </Box>
     </Box>
