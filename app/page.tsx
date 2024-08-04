b25567f623335c89d5854199c1c5cfd7c7273247
Kim Hyun <arkcat@outlook.com>
2024-08-04 19:06:37 +0900

    대시보드 페이지 에러 수정
    - 길드 토벌전 일정 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -1,88 +1,94 @@
-'use client'
+"use client";
 
-import { useEffect, useState } from 'react'
-import { supabase } from '@/utils/supabase'
-import { useRecoilState } from 'recoil'
-import { userAtom } from '../recoil/userAtom'
-import LoginForm from '@/components/LoginForm'
-import { Box, Grid, List, ListItem, ListItemText, Typography } from '@mui/material'
-import titleImage from './res/title.png'
-import PartyListItem, { CombinedData } from '@/components/PartyListItem'
-import { getDayOfWeek } from '@/utils/DateUtils'
+import { useEffect, useState } from "react";
+import { supabase } from "@/utils/supabase";
+import { useRecoilState } from "recoil";
+import { userAtom } from "../recoil/userAtom";
+import LoginForm from "@/components/LoginForm";
+import {
+  Box,
+  Grid,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import titleImage from "./res/title.png";
+import PartyListItem, { CombinedData } from "@/components/PartyListItem";
+import { getDayOfWeek } from "@/utils/DateUtils";
 
-export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic";
 
 export default function Index() {
-  const [showLoginForm, setShowLoginForm] = useState(false)
-  const [userState, setUserState] = useRecoilState<any | null>(userAtom)
-  const [mainComment, setMainComment] = useState<string>('')
-  const [combinedData, setCombinedData] = useState<CombinedData[]>([])
-  const [subjug, setSubjug] = useState<boolean>(false)
+  const [showLoginForm, setShowLoginForm] = useState(false);
+  const [userState, setUserState] = useRecoilState<any | null>(userAtom);
+  const [mainComment, setMainComment] = useState<string>("");
+  const [combinedData, setCombinedData] = useState<CombinedData[]>([]);
+
   useEffect(() => {
     const fetchMainComment = async () => {
       try {
         const { data, error } = await supabase
-          .from('Member')
-          .select('comment')
-          .eq('id', 9999)
+          .from("Member")
+          .select("comment")
+          .eq("id", 9999);
 
         if (error) {
-          throw error
+          throw error;
         }
 
         if (data.length > 0) {
-          setMainComment(data[0].comment)
+          setMainComment(data[0].comment);
         }
       } catch (error: any) {
-        console.error("코멘트 불러오기 오류 : ", error)
+        console.error("코멘트 불러오기 오류 : ", error);
       }
-    }
+    };
 
     const fetchPartyData = async () => {
       // PartyData 가져오기
-      const today = getDayOfWeek()
-      if (today == 4) {
-        setSubjug(true)
-      }
+      const today = getDayOfWeek();
 
       const { data: partyData, error: partyError } = await supabase
-        .from('Party')
-        .select('*')
-        .eq('day', today)
-        .order('time')
+        .from("Party")
+        .select("*")
+        .eq("day", today)
+        .order("time");
 
       if (partyError) {
-        console.error('Error fetching party data:', partyError);
+        console.error("Error fetching party data:", partyError);
         return;
       }
 
       // RaidData 가져오기
       const { data: raidData, error: raidError } = await supabase
-        .from('Raid')
-        .select('*')
-        .order('id')
+        .from("Raid")
+        .select("*")
+        .order("id");
 
       if (raidError) {
-        console.error('Error fetching raid data:', raidError);
+        console.error("Error fetching raid data:", raidError);
         return;
       }
 
       // MemberData 가져오기
-      const characterIds = partyData.flatMap(party => party.member);
+      const characterIds = partyData.flatMap((party) => party.member);
       const { data: characterData, error: characterError } = await supabase
-        .from('Character')
-        .select('*')
-        .in('id', characterIds);
+        .from("Character")
+        .select("*")
+        .in("id", characterIds);
 
       if (characterError) {
-        console.error('Error fetching character data:', characterError);
+        console.error("Error fetching character data:", characterError);
         return;
       }
 
       // 조합된 데이터 생성
-      const combinedData = partyData.map(party => {
+      const combinedData = partyData.map((party) => {
         const raid = raidData[party.raid_id];
-        const members = characterData.filter(character => party.member.includes(character.id));
+        const members = characterData.filter((character) =>
+          party.member.includes(character.id)
+        );
 
         return {
           party,
@@ -91,112 +97,164 @@ export default function Index() {
         };
       });
 
-      setCombinedData(combinedData)
-    }
+      setCombinedData(combinedData);
+    };
 
-    fetchMainComment()
-    fetchPartyData()
-  }, [])
+    fetchMainComment();
+    fetchPartyData();
+  }, []);
 
   const handleUpdateMember = async (uid: string) => {
     try {
       if (!userState) {
         const { data, error } = await supabase
-          .from('Member')
+          .from("Member")
           .select()
-          .eq('uid', uid)
+          .eq("uid", uid);
 
         if (error) {
-          throw error
+          throw error;
         }
 
         if (data.length > 0) {
-          setUserState(data[0])
-          setShowLoginForm(false)
+          setUserState(data[0]);
+          setShowLoginForm(false);
         }
       }
     } catch (error: any) {
-      console.error('Error updating member:', error.message)
+      console.error("Error updating member:", error.message);
     }
-  }
+  };
 
   return (
-    <Box style={{ display: "flex", position: "relative", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100dvh" }}>
-      <Box display="flex" position="relative" flexDirection="column" alignItems="center" justifyContent="center" gap={3}>
+    <Box
+      style={{
+        display: "flex",
+        position: "relative",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100dvh",
+      }}
+    >
+      <Box
+        display="flex"
+        position="relative"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        gap={3}
+      >
         <Box>
-          <img src={titleImage.src} className='title-image' alt="Title" />
+          <img src={titleImage.src} className="title-image" alt="Title" />
         </Box>
         <Box>
-          {!userState &&
-            (
-              <div className='outlined'>
-                {showLoginForm === false ?
-                  <div onClick={() => setShowLoginForm(true)} style={{ cursor: 'pointer', fontFamily: 'NanumBarunGothic', fontSize: '30px' }}>로그인</div> :
-                  <div className="loginForm">
-                    <LoginForm onLoginSuccess={handleUpdateMember} />
-                  </div>
-                }
-              </div>
-            )}
+          {!userState && (
+            <div className="outlined">
+              {showLoginForm === false ? (
+                <div
+                  onClick={() => setShowLoginForm(true)}
+                  style={{
+                    cursor: "pointer",
+                    fontFamily: "NanumBarunGothic",
+                    fontSize: "30px",
+                  }}
+                >
+                  로그인
+                </div>
+              ) : (
+                <div className="loginForm">
+                  <LoginForm onLoginSuccess={handleUpdateMember} />
+                </div>
+              )}
+            </div>
+          )}
         </Box>
         <Box>
-          {userState &&
-            (<Box>
+          {userState && (
+            <Box>
               <Grid container spacing={2}>
-                <Grid item xs={12} md={6} >
-                  <Typography variant="h4"
-                    textAlign={'center'}
+                <Grid item xs={12} md={6}>
+                  <Typography
+                    variant="h4"
+                    textAlign={"center"}
                     sx={{ mt: 2, mb: 2 }}
-                    style={{ fontFamily: 'SUIT-Regular', fontSize: '32px', fontWeight: '600' }}>오늘의 공대</Typography>
-                  <div className='main-post'>
+                    style={{
+                      fontFamily: "SUIT-Regular",
+                      fontSize: "32px",
+                      fontWeight: "600",
+                    }}
+                  >
+                    오늘의 공대
+                  </Typography>
+                  <div className="main-post">
                     <List dense>
-                      {subjug === true && (
+                      {combinedData.map((data) => (
+                        <PartyListItem
+                          key={data.party.id}
+                          memberId={userState.id}
+                          data={data}
+                        />
+                      ))}
+                      {combinedData.length === 0 && (
                         <ListItem>
                           <ListItemText
                             primary={
-                              <Box style={{ display: 'flex', alignItems: 'center', fontFamily: 'NanumBarunGothic', fontSize: '20px' }}>
-                                <span style={{ lineHeight: '20px', color: 'green' }}>{`21:20`}</span>
-                                <span style={{ lineHeight: '20px', paddingLeft: '10px' }}>{`길드 토벌전`}</span>
+                              <Box
+                                style={{
+                                  display: "flex",
+                                  alignItems: "center",
+                                  fontFamily: "NanumBarunGothic",
+                                  fontSize: "20px",
+                                }}
+                              >
+                                <span
+                                  style={{
+                                    lineHeight: "20px",
+                                    paddingLeft: "10px",
+                                  }}
+                                >{`오늘은 일정이 없어요`}</span>
                               </Box>
                             }
                           />
                         </ListItem>
                       )}
-                      {combinedData.map((data) => (
-                        <PartyListItem key={data.party.id} memberId={userState.id} data={data} />
-                      ))}
-                      {combinedData.length === 0 && subjug === false && (
-                        <ListItem>
-                        <ListItemText
-                          primary={
-                            <Box style={{ display: 'flex', alignItems: 'center', fontFamily: 'NanumBarunGothic', fontSize: '20px' }}>
-                              <span style={{ lineHeight: '20px', paddingLeft: '10px' }}>{`오늘은 일정이 없어요`}</span>
-                            </Box>
-                          }
-                        />
-                      </ListItem>
-                      )}
                     </List>
                   </div>
                 </Grid>
                 <Grid item xs={12} md={6}>
-                  <Typography variant="h4"
-                    textAlign={'center'}
+                  <Typography
+                    variant="h4"
+                    textAlign={"center"}
                     sx={{ mt: 2, mb: 2 }}
-                    style={{ fontFamily: 'SUIT-Regular', fontSize: '32px', fontWeight: '600' }}>운영진 공지</Typography>
-                  <div className='main-post'>
-                    {mainComment.split('\n').map((line, index) => (
-                      <Typography key={index} style={{ fontFamily: 'NanumBarunGothic', fontSize: '20px', textAlign: 'center' }}>
+                    style={{
+                      fontFamily: "SUIT-Regular",
+                      fontSize: "32px",
+                      fontWeight: "600",
+                    }}
+                  >
+                    운영진 공지
+                  </Typography>
+                  <div className="main-post">
+                    {mainComment.split("\n").map((line, index) => (
+                      <Typography
+                        key={index}
+                        style={{
+                          fontFamily: "NanumBarunGothic",
+                          fontSize: "20px",
+                          textAlign: "center",
+                        }}
+                      >
                         {line}
                       </Typography>
                     ))}
                   </div>
                 </Grid>
               </Grid>
-            </Box>)}
+            </Box>
+          )}
         </Box>
       </Box>
     </Box>
-  )
+  );
 }
-
